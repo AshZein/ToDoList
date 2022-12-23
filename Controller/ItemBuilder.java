0811dd3090085a78ca223591217dd30f0aa04f12
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-23 17:53:20 -0500

    Complete base ItemBuilder with setters

--- Controller/ItemBuilder.java
+++ Controller/ItemBuilder.java
@@ -1,2 +1,32 @@
-package Controller;public class ItemBuilder {
+package Controller;
+
+import Model.Item;
+
+public class ItemBuilder {
+    Item item;
+
+    public ItemBuilder(){
+        item = new Item();
+    }
+
+    public void setDate(String date){
+        item.setDate(date);
+    }
+
+    public void setTime(String time){
+        item.setTime(time);
+    }
+    public void setTitle(String title){
+        item.setTitle(title);
+    }
+    public void setDescription(String description){
+        item.setDescription(description);
+    }
+    public void setId(String id){
+
+    }
+
+    public Item getItem(){
+        return this.item;
+    }
 }
