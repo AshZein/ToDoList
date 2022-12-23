ed822fb5133e1fa875291e6def94bf3799376fa8
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-23 17:54:01 -0500

    method for adding new items to the ItemList

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,4 +1,34 @@
 package Controller;
 
+import Model.ItemList;
+
+import java.util.HashMap;
+
 public class Controller {
+    ItemList itemList;
+
+    public Controller(){
+        itemList = new ItemList();
+    }
+
+    public String addItem(HashMap<String, String> parameters){
+        ItemBuilder iBuild = new ItemBuilder();
+
+        iBuild.setDate(parameters.get("date"));
+        iBuild.setTime(parameters.get("time"));
+        iBuild.setDescription(parameters.get("description"));
+        iBuild.setTitle(parameters.get("title"));
+
+        String id = generateId();
+        iBuild.setId(id);
+
+        // adding item to the ItemList
+        itemList.addItem(id, iBuild.getItem());
+
+        return id;
+    }
+
+    private String generateId(){
+        throw new UnsupportedOperationException("Not complete yet");
+    }
 }
