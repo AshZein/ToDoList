16e6a6b09d9e64827f436ccec909fb98ba831581
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-31 16:44:17 -0500

    Font size attribute

--- Views/UIDimensions.java
+++ Views/UIDimensions.java
@@ -1,9 +1,12 @@
 package Views;
 
+import javafx.scene.text.Font;
+
 public class UIDimensions {
     // all dimensions are {x, y}
     static double[] categoryDim = new double[] {316, 100};
-    static double[] stageDim = new double[] {950, 750};
+    static Font categoryFont = new Font(35);
+    public static double[] stageDim = new double[] {950, 750};
     static double[] itemViewDim = new double[]{634, 750};
     static double[] mainViewDim = new double[]{950, 750};
     static double[] listViewDim = new double[]{316, 750};
