13b6451c47f3e81a2d4c2677576be8e81161706e
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-31 17:29:21 -0500

    Stackpane dimensions and styling

--- Views/CategoryView.java
+++ Views/CategoryView.java
@@ -1,16 +1,12 @@
 package Views;
 
 import javafx.geometry.Pos;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class CategoryView {
     String name;
-    Canvas canvas;
-    GraphicsContext gc;
+
 
     public CategoryView(String name){
         this.name = name;
@@ -20,22 +16,15 @@ public class CategoryView {
         double[] dimension = UIDimensions.categoryDim;
 
         StackPane stackOut = new StackPane();
-        stackOut.setStyle("-fx-background-color: #000000");
+        stackOut.setPrefWidth(dimension[0]);
+        stackOut.setPrefHeight(dimension[1]);
+        stackOut.setStyle("-fx-background-color: #000000;");
         stackOut.setAlignment(Pos.CENTER);
 
         Text text = new Text(this.name);
         text.setFont(UIDimensions.categoryFont);
         text.setStyle("-fx-stroke: #ffffff; -fx-stroke-width: 1.5; -fx-fill: #ffffff");
         stackOut.getChildren().add(text);
-
-        canvas = new Canvas(dimension[0], dimension[1]);
-
-        gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.WHITE);
-        gc.strokeRect(0, 0, dimension[0], dimension[1]);
-        stackOut.getChildren().add(canvas);
-
-
 
         return stackOut;
     }
