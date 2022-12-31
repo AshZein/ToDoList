ba5a1d255b876fa2cc1fda74dd486a6285098c4e
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-31 16:44:50 -0500

    setting up the root

--- Views/MainView.java
+++ Views/MainView.java
@@ -1,7 +1,35 @@
 package Views;
 
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MainView {
+    VBox toDoLists;
+    HBox root;
 
+    public MainView(){
+        toDoLists = new VBox(0);
+        toDoLists.setPadding(new Insets(4,4,4,4));
+//        toDoLists.setStyle("-fx-background-color: #000000");
+        toDoLists.setAlignment(Pos.CENTER);
+
+        root = new HBox(20);
+        root.setPadding(new Insets(4,4,4,4));
+        root.setStyle("-fx-background-color: #ffffff");
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(toDoLists);
+    }
+
+    public void addToDoList(Node toDoList){
+        toDoLists.getChildren().add(toDoList);
+    }
+
+    public Scene getRoot(){
+        return new Scene(root, UIDimensions.stageDim[0], UIDimensions.stageDim[1]);
+    }
 }
