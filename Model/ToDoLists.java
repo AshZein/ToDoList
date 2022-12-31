da9fc2f97e0809a76de42c2e82bd595864fb15ea
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-31 14:49:13 -0500

    Created ToDoLists class and the method createList

---
+++ Model/ToDoLists.java
@@ -0,0 +1,22 @@
+package Model;
+
+import Exceptions.ListAlreadyExistsException;
+
+import java.util.HashMap;
+
+public class ToDoLists {
+    HashMap<String, ItemList> lists = new HashMap<>();
+
+    /*
+     * Creates a new ItemList to add to the list of ItemLists. Throws an exception if there is already a list using the
+     * name given.
+     */
+    public void createList(String name) throws ListAlreadyExistsException {
+        if (lists.containsKey(name)){
+            throw new ListAlreadyExistsException("This List already exists");
+        }
+        else{
+            lists.put(name, new ItemList());
+        }
+    }
+}
