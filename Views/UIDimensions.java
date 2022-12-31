cbc446f483544fab82e654604d656122a3484d66
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2022-12-31 15:12:09 -0500

    set default dimension values

--- Views/UIDimensions.java
+++ Views/UIDimensions.java
@@ -1,9 +1,10 @@
 package Views;
 
 public class UIDimensions {
-    int[] categoryDim = new int[2];
-    int[] stageDim = new int[2];
-    int[] itemViewDim = new int[2];
-    int[] mainViewDim = new int[2];
-    int[] listViewDim = new int[2];
+    // all dimensions are {x, y}
+    int[] categoryDim = new int[] {316, 100};
+    int[] stageDim = new int[] {950, 750};
+    int[] itemViewDim = new int[]{634, 750};
+    int[] mainViewDim = new int[]{950, 750};
+    int[] listViewDim = new int[]{316, 750};
 }
