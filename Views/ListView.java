384fa30650a38c6efeee5f17170a9eaa8edaab2c
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2023-01-01 17:28:01 -0500

    List view draw method

--- Views/ListView.java
+++ Views/ListView.java
@@ -1,10 +1,70 @@
 package Views;
 
-import Model.ToDoLists;
+import Model.Item;
+import Model.ItemList;
+import javafx.geometry.Pos;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class ListView {
+    ItemList itemList;
+    String title;
 
+    public ListView(ItemList itemList){
+        this.itemList = itemList;
+        title = itemList.getTitle();
+    }
 
-    public ListView(){
+    public VBox drawListView(){
+        VBox out = new VBox();
+        out.setStyle("-fx-background-color: #000000;");
+        out.setMaxSize(UIDimensions.listViewDim[0], UIDimensions.listViewDim[1]);
+
+        StackPane header = new StackPane();
+        header.setMaxSize(UIDimensions.itemViewDim[0], UIDimensions.itemViewDim[1] * 1.5);
+        header.setStyle("-fx-background-color: #000000;");
+        header.setAlignment(Pos.BASELINE_LEFT);
+
+        Text headerText = new Text(title);
+        headerText.setFont(UIDimensions.listTitleFont);
+        headerText.setStyle("-fx-stroke: #ffffff; -fx-stroke-width: 1.5; -fx-fill: #ffffff");
+        headerText.setTextAlignment(TextAlignment.LEFT);
+
+        header.getChildren().add(headerText);
+
+        out.getChildren().add(header);
+        out.getChildren().add(getListLine());
+
+        for(String itemId: itemList.getCompleted().keySet()){
+            Item item = itemList.getItem(itemId);
+            ItemView itemView = new ItemView(item);
+
+            out.getChildren().add(itemView.drawItemView());
+            out.getChildren().add(getListLine());
+        }
+
+        for(String itemId: itemList.getIncomplete().keySet()){
+            Item item = itemList.getItem(itemId);
+            ItemView itemView = new ItemView(item);
+
+            out.getChildren().add(itemView.drawItemView());
+            out.getChildren().add(getListLine());
+        }
+
+        return out;
+    }
+
+    private Line getListLine(){
+        Line line = new Line();
+        line.setStartX(0.0f);
+        line.setStartY(0.0f);
+        line.setEndX(UIDimensions.listViewDim[0] * 2);
+        line.setEndY(0);
+        line.setStyle("-fx-fill: #808080; -fx-stroke: #808080;");
+
+        return line;
     }
 }
