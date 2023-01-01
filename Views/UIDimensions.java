7fbb50321e9241ab52be5a8180eed68c6be9e270
Ashkan Zeinolabedini <ashkan-oakville@hotmail.com>
2023-01-01 17:30:24 -0500

    added new sizes

--- Views/UIDimensions.java
+++ Views/UIDimensions.java
@@ -6,8 +6,10 @@ public class UIDimensions {
     // all dimensions are {x, y}
     static double[] categoryDim = new double[] {316, 100};
     static Font categoryFont = new Font(35);
+    static Font[] itemFont = new Font[] {new Font(18), new Font(12)};  // {title font, dateTime font}
+    static Font listTitleFont = new Font(40);
     public static double[] stageDim = new double[] {950, 750};
-    static double[] itemViewDim = new double[]{634, 750};
+    static double[] itemViewDim = new double[]{634, 100};
     static double[] mainViewDim = new double[]{950, 750};
     static double[] listViewDim = new double[]{316, 750};
 }
